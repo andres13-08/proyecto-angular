580b4b879e4a37d58b4650629e567f2a14a5e17b
andres13-08 <andres.becerra13@hotmail.com>
2025-11-04 18:33:53 -0500

    Ajustes

--- clase_6/src/app/app-routing-module.ts
+++ clase_6/src/app/app-routing-module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./featured/dashboard/dashboard-module').then((m) => m.DashboardModule),
   },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
--- clase_6/src/app/featured/dashboard/courses/courses-module.ts
+++ clase_6/src/app/featured/dashboard/courses/courses-module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing-module';
 import { Courses } from './courses';
+import { CoursesTable } from './courses-table/courses-table';
+import { CoursesForm } from './courses-form/courses-form';
 import { SharedModule } from '../../../shared/shared-module';
 
 @NgModule({
-  declarations: [Courses],
+  declarations: [Courses, CoursesTable, CoursesForm],
   imports: [CommonModule, CoursesRoutingModule, SharedModule],
 })
 export class CoursesModule {}
